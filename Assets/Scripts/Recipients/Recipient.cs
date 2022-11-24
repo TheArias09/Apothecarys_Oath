32a131c2b0612a52f2c674ce8a7446b5d50a53e2
Totoo98 <thomasbessieres@orange.fr>
2022-11-24 12:15:06 +0100

    Fix flowing relative to shader parameter names update.

--- Assets/Scripts/Recipients/Recipient.cs
+++ Assets/Scripts/Recipients/Recipient.cs
@@ -26,7 +26,7 @@ namespace Recipients
             if (affectsShader)
             {
                 Debug.Log("Update with: " + (quantity * 0.2f - 0.1f).ToString());
-                rend.material.SetFloat("_Fill", quantity * 0.2f - 0.1f);
+                rend.material.SetFloat("_FillV2", quantity * 0.2f - 0.1f);
             }
         }
 
