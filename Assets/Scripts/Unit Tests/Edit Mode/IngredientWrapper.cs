3dfa5a7cdde9f0a14fe620482590fbea0291c1a5
nethos <geeklego@gmail.com>
2022-11-30 11:28:47 +0100

    added ingredient wrapper

---
+++ Assets/Scripts/Unit Tests/Edit Mode/IngredientWrapper.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientWrapper : MonoBehaviour
+{
+    [SerializeField] private Ingredient ingredient;
+}
