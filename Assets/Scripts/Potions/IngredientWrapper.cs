8e33d980c1b04ed67f17ba4275291677cb88fe99
nethos <geeklego@gmail.com>
2022-12-01 16:07:40 +0100

    clean code

--- Assets/Scripts/Potions/IngredientWrapper.cs
+++ Assets/Scripts/Potions/IngredientWrapper.cs
@@ -87,7 +87,7 @@ public class IngredientWrapper : MonoBehaviour
             float removedQty = Mathf.Min(ing.Quantity, deltaQty * ing.Quantity / quantity);
             ing.Quantity -= removedQty;
 
-            Ingredient pouredIngredient = new Ingredient(ing.Name, removedQty, ing.Quality, ing.Color, ing.Cures);
+            Ingredient pouredIngredient = new (ing.Name, removedQty, ing.Quality, ing.Color, ing.Cures);
             pouredIngredients.Add(pouredIngredient);
         }
 
