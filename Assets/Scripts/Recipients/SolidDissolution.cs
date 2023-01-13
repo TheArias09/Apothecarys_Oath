1b40e90734657214c61297088abceca42ddf3a9d
Totoo98 <thomasbessieres@orange.fr>
2023-01-13 16:38:53 +0100

    Update Solid Dissolution so that it gets poured smoothly.

--- Assets/Scripts/Recipients/SolidDissolution.cs
+++ Assets/Scripts/Recipients/SolidDissolution.cs
@@ -9,18 +9,70 @@ public class SolidDissolution : MonoBehaviour
     [SerializeField] List<Ingredient> ingredients;
     [SerializeField] string collisionTag = "Bottleneck";
 
-    [SerializeField] UnityEvent OnDissolution;
+    [SerializeField] UnityEvent OnDissolutionStart;
+    [SerializeField] UnityEvent OnDissolutionEnd;
+
+    [SerializeField] float pourSpeed = 2f;
+
+    private bool hasAlreadyBeenTriggered = false;
 
     private void OnTriggerEnter(Collider other)
     {
+        if (hasAlreadyBeenTriggered) return;
+
         if (other.gameObject.CompareTag(collisionTag))
         {
-            var targetIngredientWrapper = other.GetComponentInParent<IngredientWrapper>();
-            var totalQuantity = ingredients.Sum(ing => ing.Quantity);
-            targetIngredientWrapper.FillWith(ingredients, totalQuantity);
-
-            OnDissolution?.Invoke();
-            Destroy(gameObject);
+            StartCoroutine(Dissolve(other));
         }
+    }
+
+    private IEnumerator Dissolve(Collider other)
+    {
+        hasAlreadyBeenTriggered = true;
+
+        var targetIngredientWrapper = other.GetComponentInParent<IngredientWrapper>();
+        var totalQuantity = ingredients.Sum(ing => ing.Quantity);
+
+        OnDissolutionStart?.Invoke();
+
+        var quantityToPourLeft = totalQuantity; 
+        while(totalQuantity > 0f)
+        {
+            var quantityMaxToPourThisFrame = pourSpeed * Time.deltaTime;
+            var quantityToPourThisFrame = 0f;
+
+            ingredients.RemoveAll(ing => ing.Quantity == 0);
+
+            if (quantityToPourLeft < quantityMaxToPourThisFrame)
+            {
+                quantityToPourThisFrame = quantityToPourLeft;
+                quantityToPourLeft = 0f;
+            }
+            else
+            {
+                quantityToPourThisFrame = quantityMaxToPourThisFrame;
+                quantityToPourLeft -= quantityMaxToPourThisFrame;
+            }
+
+            List<Ingredient> pouredIngredientsThisFrame = new();
+
+            foreach (var ing in ingredients)
+            {
+                float removedQty = Mathf.Min(ing.Quantity, quantityToPourThisFrame * ing.Quantity / totalQuantity);
+                ing.Quantity -= removedQty;
+
+                Ingredient pouredIngredient = new(ing.Name, removedQty, ing.Quality, ing.Color, ing.Cures);
+                pouredIngredientsThisFrame.Add(pouredIngredient);
+            }
+
+            targetIngredientWrapper.FillWith(pouredIngredientsThisFrame, quantityToPourThisFrame);
+
+            totalQuantity = ingredients.Sum(ing => ing.Quantity);
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        OnDissolutionStart?.Invoke();
+        Destroy(gameObject);
     }
 }
