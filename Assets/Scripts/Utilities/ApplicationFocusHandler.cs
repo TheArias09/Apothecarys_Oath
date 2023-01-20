26e4924403b174f45b8e532db33ecf9fdb05c99a
Totoo98 <thomasbessieres@orange.fr>
2023-01-20 16:06:35 +0100

    Update Potion Grimoire Layout.

---
+++ Assets/Scripts/Utilities/ApplicationFocusHandler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ApplicationFocusHandler : MonoBehaviour
+{
+    [SerializeField] UnityEvent OnPause;
+    [SerializeField] UnityEvent OnResume;
+
+    private float baseTimeScale = 1f;
+    private float pauseTimeScale = 0f;
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if(focus)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    private void Resume()
+    {
+        Time.timeScale = baseTimeScale;
+        OnResume?.Invoke();
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = pauseTimeScale;
+        OnPause?.Invoke();
+    }
+}
