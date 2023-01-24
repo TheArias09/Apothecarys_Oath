fa3907997b044e07151a4429953349a2937c929a
Totoo98 <thomasbessieres@orange.fr>
2023-01-24 15:51:52 +0100

    Grimoire Appear with a HELP potion.

--- Assets/Scripts/Potions/StartStopPotion.cs
+++ Assets/Scripts/Potions/StartStopPotion.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(IngredientWrapper))]
 public class StartStopPotion : MonoBehaviour
 {
-    [SerializeField] private bool start;
+    public enum MenuPotionType { Start, Stop, Help }
+
+    [SerializeField] MenuPotionType type;
+
     [SerializeField] private float triggerQuantity;
     [SerializeField] private GameObject uiText;
 
     private IngredientWrapper ingredientWrapper;
+
+    [SerializeField] UnityEvent OnHelpEvent;
 
     private void Start()
     {
@@ -24,8 +30,18 @@ public class StartStopPotion : MonoBehaviour
         {
             Debug.Log("Game potion triggered");
 
-            if (start) GameManager.Instance.StartGame();
-            else GameManager.Instance.QuitGame();
+            if (type == MenuPotionType.Start)
+            {
+                GameManager.Instance.StartGame();
+            }
+            else if (type == MenuPotionType.Stop)
+            {
+                GameManager.Instance.QuitGame();
+            }
+            else if (type == MenuPotionType.Help)
+            {
+                OnHelpEvent?.Invoke();
+            }
 
             uiText.SetActive(false);
             Destroy(gameObject);
